c851645a138093b6407b5ea5f98818c5a672882e
fspataro <spataroFranco1@gmail.com>
2023-06-13 21:52:12 -0300

    Agrego svg

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -1,19 +1,25 @@
 import logo from "../../../assets/logo.jpg";
 import logoWhatsapp from "../../../assets/whatsapp.svg";
+import logoIg from "../../../assets/instagram.svg";
+import logoFb from "../../../assets/facebook.svg";
 import "./s.css";
 import React from "react";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <img src={logo} alt="" className="logo-footer"/>
+      <img src={logo} alt="" className="logo-footer" />
       <p>Limbo Finca y Bodega | Mendoza, Argentina</p>
       <p>Lorem ipsum 7777, San Rafael, Mendoza</p>
       <div>
         <p>000-000-000</p>
         <p>email@email.com</p>
       </div>
-      <img src={logoWhatsapp} alt="" className="logo-whatsapp"/>
+      <div>
+        <img src={logoWhatsapp} alt="" className="logo-whatsapp" />
+        <img src={logoIg} alt="" className="logo-whatsapp" />
+        <img src={logoFb} alt="" className="logo-whatsapp" />
+      </div>
     </div>
   );
 };
